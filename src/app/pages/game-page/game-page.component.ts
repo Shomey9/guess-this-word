27e167618fe05601797a9eec85970a90b4396f42
Ady Rendusara <hadyanrendusara@yahoo.com>
2024-02-02 13:28:05 -0500

    Run prettier

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -1,25 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'game-page',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './game-page.component.html',
-  styleUrl: './game-page.component.css'
+  styleUrl: './game-page.component.css',
 })
 export class GamePageComponent {
-  gameWord = "placeholder"
+  gameWord = 'placeholder';
 
   formGroup = this.formBuilder.group({
-    userInput: [null, this.wordValidator(this.gameWord)]
-  })
+    userInput: [null, this.wordValidator(this.gameWord)],
+  });
 
   wordValidator(answer: string): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-      return control.value === answer ? null : { incorrectAnswer: { value: control.value } }
-    }
+      return control.value === answer ? null : { incorrectAnswer: { value: control.value } };
+    };
   }
 
   constructor(private formBuilder: FormBuilder) {}

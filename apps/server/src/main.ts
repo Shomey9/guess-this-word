db14f428f4a98c1c08257ef7d106b64431e4765b
Ady Rendusara <hadyanrendusara@yahoo.com>
2024-02-03 00:21:05 -0500

    Restructure file tree

--- apps/server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ServerModule } from './server.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ServerModule);
-  await app.listen(3000);
-}
-bootstrap();
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GamePageComponent } from './pages/game-page/game-page.component';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, GamePageComponent, AppComponent, ReactiveFormsModule
+  ]
+})
+export class AppModule { }
